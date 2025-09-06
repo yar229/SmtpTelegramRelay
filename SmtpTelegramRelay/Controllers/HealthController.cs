3e08045ed46f50ed8a822651db461a8fc99b8a83
YaR <yar229@yandex.ru>
2025-09-06 07:50:05 +0300

    health endpoint

---
+++ SmtpTelegramRelay/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmtpTelegramRelay.Models;
+
+namespace SmtpTelegramRelay.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    public HealthController()
+    {
+    }
+
+    [HttpGet()]
+    public async Task<IActionResult> Alive([FromQuery] WebMessage webMessage, [FromForm] IFormCollection formCollection)
+    {
+        return Ok();
+    }
+}
