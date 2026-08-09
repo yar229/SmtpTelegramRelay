d20d603740887ce5b61e305a70daa586a2066d26
YaR <yar229@yandex.ru>
2026-08-09 20:07:35 +0300

    add swagger

--- SmtpTelegramRelay/Startup.cs
+++ SmtpTelegramRelay/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.OpenApi;
 using SmtpTelegramRelay.Configuration;
 using SmtpTelegramRelay.Services;
 using SmtpTelegramRelay.Services.TelegramStores;
@@ -23,6 +24,12 @@ public class Startup
             .AddControllers();
 
         services
+            .AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SmtpTelegramRelay", Version = "v1" });
+            });
+
+        services
             .AddSingleton<TelegramStore>()
             .AddSingleton<SmtpServerBuilder>()
             .AddSingleton<Relay>()
@@ -41,6 +48,8 @@ public class Startup
             $"({Environment.UserDomainName}\\{Environment.UserName}) " +
             $"Platform: {Environment.OSVersion.Platform} {Environment.OSVersion.VersionString}");
         
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SmtpTelegramRelay API"));
         app.UseRouting();
         app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
     }
